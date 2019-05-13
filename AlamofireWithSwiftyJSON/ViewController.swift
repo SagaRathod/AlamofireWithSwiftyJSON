27ae0f2535e40198468200a6ad3afd4065a70dc0
SagarSRathode <32382376+SagaRathod@users.noreply.github.com>
2019-05-13 17:55:16 +0530

    Add files via upload

---
+++ AlamofireWithSwiftyJSON/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  AlamofireWithSwiftyJSON
+//
+//  Created by Sagar  on 5/13/19.
+//  Copyright © 2019 Sagar. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource  {
+
+    @IBOutlet var tblJSON: UITableView!
+    var arrRes = [[String:AnyObject]]() //Array of dictionary
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Alamofire.request("http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["contacts"].arrayObject {
+                    self.arrRes = resData as! [[String:AnyObject]]
+                }
+                if self.arrRes.count > 0 {
+                    self.tblJSON.reloadData()
+                }
+            }
+        }
+ 
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrRes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "jsonCell")!
+        var dict = arrRes[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = dict["name"] as? String
+        cell.detailTextLabel?.text = dict["email"] as? String
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # AlamofireWithSwiftyJSON
+Sample of Alamofire with SwiftyJSON
+
+Alamofire 4.x
+Swift 5
+Xcode 10.1
+iOS 12.1
+
